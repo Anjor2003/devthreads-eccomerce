181c5a0d03de6c51118b24763839787b0b2cecd1
Anjor2003 <101336015+Anjor2003@users.noreply.github.com>
2024-02-07 12:43:11 +0100

    Update getOrders.tsx

--- app/actions/getOrders.tsx
+++ app/actions/getOrders.tsx
@@ -3,7 +3,7 @@ import prisma from "@/lib/prismadb";
 export const getOrders = async (user: any) => {
   const orders = await prisma.order.findMany({
     where: {
-      userId: user.id
+      userId: user?.id
     },
     include: {items: true}
   });
